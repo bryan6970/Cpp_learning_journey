3ee7934f3dc9b23ad940cf7d6ff293bdaf3b0824
bryan6970 <thacubingguy@gmail.com>
2023-03-12 17:49:25 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    int x = 1;
+    int y = 5;
+
+    bool b = x < 4;
+
+//    cout << y;
+
+    y = ++x;
+
+//    cout << y << ' ' << x;
+
+    cout << 1 && 1;
+
+    return 0;
+}
+
+

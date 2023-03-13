7378dc2143e7c03ee112071d83d15e44feed9bb5
bryan6970 <thacubingguy@gmail.com>
2023-03-13 13:44:29 +0800

    Used classes in simple_bank.cpp

--- Simple_bank/simple_bank.cpp
+++ Simple_bank/simple_bank.cpp
@@ -1,36 +1,44 @@
 //
 // Created by Bryan on 12/3/2023.
 //
+//
+// Updated with classes on 13/3/23
+//
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-
-int main(){
-
+class Bank{
+public:
     long double deposit_money = 0;
 
     long double withdraw_money = 0;
 
     long double account_balance = 100;
 
+};
+
+int main(){
+
+    Bank My_bank;
 
     cout << "How much money do you want to take out?" << endl;
-    cin >> withdraw_money;
+    cin >> My_bank.withdraw_money;
 
-    account_balance -= withdraw_money;
-    withdraw_money = 0;
+    My_bank.account_balance -= My_bank.withdraw_money;
+    My_bank.withdraw_money = 0;
 
-    cout << "Your account balance is: " << account_balance << endl;
+    cout << "Your account balance is: " << My_bank.account_balance << endl;
 
     cout << "How much money do you want to deposit?" << endl;
-    cin >> deposit_money;
+    cin >> My_bank.deposit_money;
 
-    account_balance += deposit_money;
-    deposit_money = 0;
+    My_bank.account_balance += My_bank.deposit_money;
+    My_bank.deposit_money = 0;
 
-    cout << "Your account balance is: " << account_balance << endl;
+    cout << "Your account balance is: " << My_bank.account_balance << endl;
 
 
 
